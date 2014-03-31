dd0fab06284f72c5577f09dffa9e98db03d238f3
Florian Quattrocchi <florianquattrocchi@gmail.com>
2014-03-31 23:33:55 +0200

    fix url

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ TODO
 - Node JS 
 - Delete tracks from the artist
 - Make sure the 10 tracks are streamable
+- Limit > 200 premières playlists
+- bug if < 200 (test with next cursor)
+- tableau annexe avec pistes / titres et noms
 
 
 - Find the best playlist for two tracks (overlapping)
--- scrap.php
+++ scrap.php
@@ -2,38 +2,65 @@
 	include_once('config/config_dev.php');
 	$id = "b45b1aa10f1ac2941910a7f0d10f8e28";
 	$offset = 0;
-	$limit = 2;
+	$limit = 5;
 	$track_id = 45719017;
 	$tour = 1;
 
-	$stop = false;
-	while(!$stop) {
+	if(isset($_GET['url']) && $_GET['url'] != '')
+		$url = $_GET['url'];
+	else {
+		echo "shit 1";
+		exit();
+	}
+
+	$req = json_decode(file_get_contents("http://api.soundcloud.com/resolve.json?url=" . $url . "&client_id=" . APP_ID));
+
+	if(!isset($req->kind) || ($req->kind != "track")) {
+		echo "This is not a playlist";
+		exit();
+	}
+
+	echo "here";
+	$track_id = $req->id;
+
+	$result = array();
+	while($offset < 200) {
 		$url = "http://api.soundcloud.com/tracks/" . $track_id . "/playlists?app_version=feefe0ab&client_id=". APP_ID . "&limit=" . $limit . "&linked_partitioning=1&offset=". $offset;
 
 		$page_xml = file_get_contents($url);
-		
 		$xml = simplexml_load_string($page_xml);
 		$json = json_encode($xml);
 		$playlists_array = json_decode($json);
-		$result = array();
+
 
 		$variable = $playlists_array->playlist;
-		foreach ($variable as $value) {
-
-			foreach ($value->tracks->track as $tr) {
-				$result[] = $tr->id;
+		foreach ($variable as $key => $value) {
+			if($value->{'track-count'} == 1) {
+				$result[] = $value->tracks->track->id;
+				//viz($value->tracks->track->id);
+			} else { 
+				foreach ($value->tracks->track as $tr) {
+					$result[] = $tr->id;
+					//viz($tr->id);
+				}
 			}
+			echo "<p>Playlist ".$tour.'</p>';
+			$tour++;
 		}
-
-		echo "<p>XXX tour = " . $tour++ . "</p>";
+		echo "<p>While ". $offset .'</p>';
 		$offset += $limit;
-		//$stop = true;
 	}
 
 	$counted = array_count_values($result);
 	asort($counted);
-	print_r($counted);
+	$rest = array_slice($counted, 1, 10, true);
 
+	$final_list = array();
+	foreach ($rest as $key => $value) {
+		$final_list[] = $key;
+	}
+
+	echo json_encode($final_list);
 
 	function viz($array) {
 		echo "<pre>VIZ";
